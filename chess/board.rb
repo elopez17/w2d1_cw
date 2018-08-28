952b18917c0e99b20f8855e322f2af701282e3a6
elopez17 <e_lo28@yahoo.com>
2018-08-27 17:30:36 -0700

    add move_piece and sugar

--- chess/board.rb
+++ chess/board.rb
@@ -1,16 +1,17 @@
 require_relative "piece.rb"
 
 class Board
-  attr_accessor :rows
+  attr_accessor :rows, :sentinel
   def initialize()
+    @sentinel = nil
     @rows = Array.new(8) { Array.new(8) }
     
     for i in 0...8 do
       for j in 0...8 do
         if i == 0 || i == 1 || i == 6 || i == 7
-          @rows[i][j] = Piece.new()
+          @rows[i][j] = Piece.new(:something, @rows, [i, j])
         else
-          @rows[i][j] = NullPiece.new()
+          @rows[i][j] = NullPiece.new(:something, @rows, [i, j])
         end
       end
     end
@@ -18,6 +19,27 @@ class Board
   end
   
   def move_piece(start_pos, end_pos)
+    if @rows[start_pos.first][start_pos.last].is_a?(NullPiece)
+      raise "There is no piece here" 
+    end
+    if end_pos[0] < 0 || end_pos[0] > 7 || start_pos[0]] < 0 || start_pos[0] > 7
+      raise "Out of bounds"
+    end
     
+    temp = @rows[start_pos.first][start_pos.last]
+    @rows[start_pos.first][start_pos.last] = @rows[end_pos.first][end_pos.last]
+    @rows[end_pos.first][end_pos.last] = temp
+    @rows[end_pos.first][end_pos.last].pos = end_pos
+    @rows[start_pos.first][start_pos.last].pos = start_pos
+  end
+  
+  def [](pos)
+    row, col = pos
+    @rows[row][col]
+  end
+  
+  def []=(pos, value)
+    row, col = pos
+    @rows[row][col] = value
   end
 end
